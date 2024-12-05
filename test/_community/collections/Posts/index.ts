abcbb96c8bbcc4c454eeb49b125b9ee047b84208
ruslan-amboss <rri@medicuja.com>
2024-12-05 16:55:12 +0100

    Field Array is collapsed automatically when typing anything inside IF admin: { initCollapsed: true }

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -12,6 +12,27 @@ export const PostsCollection: CollectionConfig = {
       name: 'title',
       type: 'text',
     },
+    {
+      name: 'answers',
+      label: 'Answers',
+      type: 'array',
+      defaultValue: [{}, {}],
+      admin: {
+        initCollapsed: true,
+      },
+      fields: [
+        {
+          type: 'row',
+          fields: [
+            {
+              name: 'answerResult',
+              label: 'Answer result',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    },
   ],
   versions: {
     drafts: true,
